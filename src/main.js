1393cbd54d335d6365d66fb5c55490cacdda5485
Cavalon Huang <cavalon.huang@belstar.com.tw>
2022-03-23 16:29:08 +0800

    INIT: Hexo Blog

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import "./plugins/fontawesome";
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import './style/reset.css';
-
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(store).use(router).mount("#app");
